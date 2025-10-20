bb64da8602d3334eed81672574481eee0280f4fe
HectorFranco-MISO <h.franco@uniandes.edu.co>
2025-10-20 01:41:24 -0500

    Test with lb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-gateway-953169391315.us-central1.run.app',
+  // apiUrl: 'https://api-gateway-953169391315.us-central1.run.app',
+  apiUrl: 'http://35.190.67.42',
   apiEndpoints: {
     products: '/products',
     warehouses: '/warehouses',
